8a4dca0b9fe8fa833685b3231853ca742113ef11
Gina Deanna <ginadeanna@gmail.com>
2025-11-15 19:18:29 -0300

    vista de materia-actividades y materia-mensajeria

--- EduClass-1/EduClass/js/sidebar-loader.js
+++ EduClass-1/EduClass/js/sidebar-loader.js
@@ -1,13 +1,25 @@
-const SIDEBAR_PATH = "./partials/sidebar.html";
-
 document.addEventListener("DOMContentLoaded", async function () {
     try {
         const sidebar = document.getElementById("sidebar");
         const template = document.createElement("template");
+
+        const path = window.location.pathname;
+
+        let SIDEBAR_PATH = "./partials/sidebar.html"; // default
+
+        if (path.includes("materia.html")) {
+            SIDEBAR_PATH = "./partials/sidebar-materia.html";
+        }
+
+          if (path.includes("materia-mensajeria.html")) {
+            SIDEBAR_PATH = "./partials/sidebar-materia-mensajeria.html";
+        }
+
         const res = await fetch(SIDEBAR_PATH);
-        const sidebarHTML = await res.text();
-        template.innerHTML = sidebarHTML;
+        const html = await res.text();
+        template.innerHTML = html;
         sidebar.appendChild(template.content);
+
     } catch (error) {
         console.error("Error cargando sidebar:", error);
     }
